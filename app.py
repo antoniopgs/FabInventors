ec98b5ad90566007beff903e1b929724d3175595
António <apgoncalves@outlook.pt>
2020-11-24 12:43:22 +0000

    Implement get() function

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-import re
-import json
+import re, json
 
 parts = {}
 start = False
-current_layer = 0
+layer, previous_x, previous_y, previous_speed = 0, 0, 0, 0
+current_type = "TRAVEL"
 
+def get(a): # Get values from regex match
+    if a == "speed":
+        try:
+            return float(info.group(1).strip().strip("F"))
+        except AttributeError:
+            return previous_speed
+    elif a == "x":
+        try:
+            return float(info.group(2).strip().strip("X"))
+        except AttributeError:
+            return previous_x
+    elif a == "y":
+        try:
+            return float(info.group(3).strip().strip("Y"))
+        except AttributeError:
+            return previous_y
+    elif a == "extrusion":
+        try:
+            return float(info.group(4).strip().strip("E"))
+        except AttributeError:
+            return float(0) # if nothing is extruded
+            
 
 with open("gcode_samples/1_peca_suporte.gcode") as file:
     data = [movement.strip("\n") for movement in file.readlines()]
 
-previous_x, previous_y, previous_speed = 0, 0, 0
-current_type = "TRAVEL"
 for i in range(len(data)):
 
     if not start:
@@ -23,7 +43,7 @@ for i in range(len(data)):
         
     else:
         if re.match(";LAYER:\d*", data[i]):
-            current_layer = int(re.match(";LAYER:(\d*)", data[i]).group(1))
+            layer = int(re.match(";LAYER:(\d*)", data[i]).group(1))
             continue
 
         elif ";TYPE:" in data[i]:
@@ -31,33 +51,15 @@ for i in range(len(data)):
 
         elif re.match("G[01]( F\d+\.?\d*)?( X\d+\.?\d*)?( Y\d+\.?\d*)?( E-?\d+\.?\d*)?", data[i]):
             info = re.match("G[01]( F\d+\.?\d*)?( X\d+\.?\d*)?( Y\d+\.?\d*)?( E-?\d+\.?\d*)?", data[i])
-            try:
-                extrusion = float(info.group(4).strip().strip("E"))
-            except AttributeError:
-                extrusion = float(0)
-            try:
-                speed = float(info.group(1).strip().strip("F"))
-            except AttributeError:
-                speed = previous_speed
-            try:
-                current_x = float(info.group(2).strip().strip("X"))
-            except AttributeError:
-                current_x = previous_x
-            try:
-                current_y = float(info.group(3).strip().strip("Y"))
-            except AttributeError:
-                current_y = previous_y
-                
+            speed, x, y, extrusion = get("speed"), get("x"), get("y"), get("extrusion")
             line = {"Current Line": i+1,
                     "Line type": current_type,
-                    "Points": [(previous_x, previous_y), (current_x, current_y)],
+                    "Points": [(previous_x, previous_y), (x, y)],
                     "Extrusion Length": extrusion,
                     "Speed": speed
                     }
-            layers[current_layer].append(line)
-            previous_x = current_x
-            previous_y = current_y
-            previous_speed = speed
+            layers[layer].append(line)
+            previous_x, previous_y, previous_speed = x, y, speed # Update previous values to current values
 
 output = json.dumps(layers, indent=4)
 print(output)
