da6224d79d57fa434f4c32f7502878db4c78e6dd
António <apgoncalves@outlook.pt>
2020-11-30 22:35:35 +0000

    Optimize get() function

--- parser.py
+++ parser.py
@@ -6,68 +6,62 @@ previous_speed = 0
 previous_x, previous_y = 0, 0 # First Line of First Layer of First Part starts at x = 0, y = 0 (home).
 mesh = None # If no mesh in file, mesh = None.
 
-def get(a): # Get values from regex match.
-    if a == "speed":
-        try:
-            return float(info.group(1).strip().strip("F"))
-        except AttributeError:
+def get(group_number): # Get values from regex match.
+    try:
+        return float(re.sub("[A-Z]", "", info.group(group_number)))
+    except TypeError:
+        if group_number == 1:
             return previous_speed
-    elif a == "x":
-        try:
-            return float(info.group(2).strip().strip("X"))
-        except AttributeError:
+        elif group_number == 2:
             return previous_x
-    elif a == "y":
-        try:
-            return float(info.group(3).strip().strip("Y"))
-        except AttributeError:
+        elif group_number == 3:
             return previous_y
-    elif a == "extrusion":
-        try:
-            return float(info.group(5).strip().strip("E"))
-        except AttributeError:
+        elif group_number == 5:
             return float(0) # If nothing is extruded.
 
-def travel(): # If line ends in a G0 Point, Line Type = TRAVEL.
+def check_travel(): # If line ends in a G0 Point, Line Type = TRAVEL.
     if "G0 " in data[i]:
         return "TRAVEL"
     
-    else:
+    elif "G1 " in data[i]:
         return current_type
             
 
 with open("gcode_samples/2_pecas_suporte.gcode") as file:
     data = [movement.strip("\n") for movement in file.readlines()]
-    for x in data:
-        if ";MESH:" in x:
-            mesh = "NONMESH" # If any mesh in file, everything before mesh is NONMESH.
-            break
+
+for x in data:
+    if ";MESH:" in x:
+        mesh = "NONMESH" # If any mesh in file, everything before mesh is NONMESH.
+        break
 
 for i in range(len(data)):
 
     if not start:
-        if ";LAYER_COUNT:" in data[i]:
+        try:
             layer_count = int(re.match(";LAYER_COUNT:(\d*)", data[i]).group(1))
             layers = {}
             for n in range(layer_count):
                 layers[n] = {"z": None,
                              "lines": []}
             start = True
+        except:
+            pass
         
     else:
         if "G0 " in data[i] or "G1 " in data[i]:
             info = re.match("G[01]( F\d+\.?\d*)?( X\d+\.?\d*)?( Y\d+\.?\d*)?( Z-?\d+\.?\d*)?( E-?\d+\.?\d*)?", data[i])
-            speed, x, y, extrusion = get("speed"), get("x"), get("y"), get("extrusion")
+            speed, x, y, extrusion = get(1), get(2), get(3), get(5)
             if extrusion == float(0): # Only worth it to check for z when there is no extrusion.
                 if info.group(4): # If z has a value, which means z is not None.
                     if not layers[0]["z"]: # Layer 0 has 2 z values. First one is for layer 0.
-                        layers[0]["z"] = float(info.group(4).strip().strip("Z")) # 
+                        layers[0]["z"] = get(4)
                     else: # Layer 0's second z value, and all further z values are for the next layer.
-                        layers[layer + 1]["z"] = float(info.group(4).strip().strip("Z")) # Z is attributed to the upcoming layer (layer + 1).
+                        layers[layer + 1]["z"] = get(4) # Z is attributed to the upcoming layer (layer + 1).
             line = {"G-Code Line Number": i+1,
                     "G-Code Line": data[i],
                     "Part Name": mesh,
-                    "Line Type": travel(),
+                    "Line Type": check_travel(),
                     "Points": [(previous_x, previous_y), (x, y)],
                     "Extrusion Length": extrusion,
                     "Speed": speed
@@ -80,7 +74,6 @@ for i in range(len(data)):
 
         elif ";LAYER:" in data[i]:
             layer = int(re.match(";LAYER:(\d*)", data[i]).group(1))
-            continue
 
         elif ";MESH:" in data[i]:
             mesh = re.match(";MESH:(.*)", data[i]).group(1)
