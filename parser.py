e18f082ed8f595865a3d2e5297fe647a8357895e
António <apgoncalves@outlook.pt>
2020-12-01 00:15:28 +0000

    Implement Generator

--- parser.py
+++ parser.py
@@ -20,37 +20,40 @@ def get(group_number): # Get values from regex match.
             return float(0) # If nothing is extruded.
 
 def check_travel(): # If line ends in a G0 Point, Line Type = TRAVEL.
-    if "G0 " in data[i]:
+    if "G0 " in pair[1]:
         return "TRAVEL"
     
-    elif "G1 " in data[i]:
+    elif "G1 " in pair[1]:
         return current_type
-            
 
-with open("gcode_samples/2_pecas_suporte.gcode") as file:
-    data = [movement.strip("\n") for movement in file.readlines()]
-
-for x in data:
+def gen_line_pairs(file):
+    iterator = iter(open(file))
+    previous_line = next(iterator)
+    for line in iterator:
+        yield previous_line.strip("\n"), line.strip("\n")
+        previous_line = line
+        
+for x in [movement.strip("\n") for movement in open("gcode_samples/2_pecas_suporte.gcode").readlines()]:
     if ";MESH:" in x:
         mesh = "NONMESH" # If any mesh in file, everything before mesh is NONMESH.
         break
 
-for i in range(len(data)):
+for pair in gen_line_pairs("gcode_samples/2_pecas_suporte.gcode"):
 
     if not start:
         try:
-            layer_count = int(re.match(";LAYER_COUNT:(\d*)", data[i]).group(1))
+            layer_count = int(re.match(";LAYER_COUNT:(\d*)", pair[1]).group(1))
             layers = {}
             for n in range(layer_count):
                 layers[n] = {"z": None,
                              "lines": []}
             start = True
         except:
-            pass
+            continue
         
     else:
-        if "G0 " in data[i] or "G1 " in data[i]:
-            info = re.match("G[01]( F\d+\.?\d*)?( X\d+\.?\d*)?( Y\d+\.?\d*)?( Z-?\d+\.?\d*)?( E-?\d+\.?\d*)?", data[i])
+        if "G0 " in pair[1] or "G1 " in pair[1]:
+            info = re.match("G[01]( F\d+\.?\d*)?( X\d+\.?\d*)?( Y\d+\.?\d*)?( Z-?\d+\.?\d*)?( E-?\d+\.?\d*)?", pair[1])
             speed, x, y, extrusion = get(1), get(2), get(3), get(5)
             if extrusion == float(0): # Only worth it to check for z when there is no extrusion.
                 if info.group(4): # If z has a value, which means z is not None.
@@ -58,8 +61,8 @@ for i in range(len(data)):
                         layers[0]["z"] = get(4)
                     else: # Layer 0's second z value, and all further z values are for the next layer.
                         layers[layer + 1]["z"] = get(4) # Z is attributed to the upcoming layer (layer + 1).
-            line = {"G-Code Line Number": i+1,
-                    "G-Code Line": data[i],
+            line = {"G-Code Line Number": "???",
+                    "G-Code Line": pair[1],
                     "Part Name": mesh,
                     "Line Type": check_travel(),
                     "Points": [(previous_x, previous_y), (x, y)],
@@ -69,14 +72,14 @@ for i in range(len(data)):
             layers[layer]["lines"].append(line)
             previous_x, previous_y, previous_speed = x, y, speed # Update previous values to current values.
 
-        elif ";TYPE:" in data[i]:
-            current_type = re.match(";TYPE:(.*)", data[i]).group(1)
+        elif ";TYPE:" in pair[1]:
+            current_type = re.match(";TYPE:(.*)", pair[1]).group(1)
 
-        elif ";LAYER:" in data[i]:
-            layer = int(re.match(";LAYER:(\d*)", data[i]).group(1))
+        elif ";LAYER:" in pair[1]:
+            layer = int(re.match(";LAYER:(\d*)", pair[1]).group(1))
 
-        elif ";MESH:" in data[i]:
-            mesh = re.match(";MESH:(.*)", data[i]).group(1)
+        elif ";MESH:" in pair[1]:
+            mesh = re.match(";MESH:(.*)", pair[1]).group(1)
             
 
 output = json.dumps(layers, indent=4)
