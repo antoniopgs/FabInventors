dd6583db707cf5a5c669c35a2c8846e651efac49
António <apgoncalves@outlook.pt>
2020-12-03 20:39:12 +0000

    Add Stuff

--- code/test.py
+++ code/test.py
@@ -1,3 +1,4 @@
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 from fab_parser import parse
 import os, json
@@ -5,9 +6,21 @@ import os, json
 samples_folder = os.getcwd().strip("code") + "gcode_samples"
 file_names = os.listdir(samples_folder)
 
+color_dict = {
+    "SUPPORT": mcolors.to_rgba("blue"),
+    "NONMESH": mcolors.to_rgba("green"),
+    "TRAVEL": mcolors.to_rgba("green"),
+    "SKIRT": mcolors.to_rgba("green"),
+    "MESH_NAME": mcolors.to_rgba("red"),
+    "WALL-OUTER": mcolors.to_rgba("orange"),
+    "WALL-INNER": mcolors.to_rgba("orange"),
+    "SKIN": mcolors.to_rgba("violet"),
+    "FILL": mcolors.to_rgba("yellow")
+}
+
 for file_name in file_names:
     layers = json.loads(parse(f"{samples_folder}/{file_name}"))
-    x, y, z = [], [], []
+    x, y, z, colors = [], [], [], []
 
     for value in layers.values():
         for line in value["lines"]:
@@ -15,7 +28,8 @@ for file_name in file_names:
                 x.append(point[0])
                 y.append(point[1])
                 z.append(value["z"])
-                 
+                colors.append(color_dict[line["Line Type"]])
+
     fig = plt.figure()
     ax = fig.gca(projection="3d")
     ax.set_title(file_name)
@@ -24,5 +38,3 @@ for file_name in file_names:
     ax.set_zlabel('z')
     ax.plot(x, y, z)
     plt.show()
-
-
