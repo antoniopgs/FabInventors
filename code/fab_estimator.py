7b52abe70fe2c12a034b1ad3c8e430c859f4833f
António <apgoncalves@outlook.pt>
2020-12-10 14:40:44 +0000

    Add Layer Changes to Estimator

--- code/fab_estimator.py
+++ code/fab_estimator.py
@@ -3,14 +3,22 @@ import datetime, math
 def estimate(data):
     time = 0
     for layer in (x for x in data["layers"].values()):
-        for line in layer["lines"]:
+        z2 = layer["z"]  # Milimeters
+        for i, line in enumerate(layer["lines"]):
+            if i != 0: # If not First Line of Layer.
+                z1 = z2
+            else: # If First Line of Layer.
+                try:
+                    z1 = data["layers"][i-1]["z"]
+                except KeyError: # If no previous layer (means start of file)
+                    z1 = 0
             x1 = line["Points"][0][0] # Milimeters
             y1 = line["Points"][0][1] # Milimeters
             x2 = line["Points"][1][0] # Milimeters
             y2 = line["Points"][1][1] # Milimeters
             speed = line["Speed"] / 60 # Milimeters Per Second
 
-            distance = math.sqrt( ((x2-x1)**2) + ((y2-y1)**2) ) # Milimeters
+            distance = math.sqrt( ((x2-x1)**2) + ((y2-y1)**2) + ((z2-z1)**2) ) # Milimeters
             time += distance / speed # Seconds
             
     return datetime.timedelta(seconds=time)
@@ -18,5 +26,4 @@ def estimate(data):
 """
 LATER:
 - Account for lines with same position as previous line.
-- Account for layer changes.
 """
