22d3b6efe8fb9414da99931cb8d8c49a6a996c79
António <apgoncalves@outlook.pt>
2021-01-03 02:14:52 +0000

    Update Slicer

--- code/fab_slicer.py
+++ code/fab_slicer.py
@@ -18,68 +18,45 @@ colors = {
 samples_folder = "./../gcode_samples/"
 gcode_file = samples_folder + "1_peca.gcode"
 
-def detect_boundaries(json):
-    for i, point in enumerate(line["points"][1] for line in json["lines"]):
-        if i != 0:
-            if point[0] < min_x:
-                min_x = point[0]
-            elif point[0] > max_x:
-                max_x = point[0]
-            if point[1] < min_y:
-                min_y = point[1]
-            elif point[1] > max_y:
-                max_y = point[1]
-        else:
-            min_x, max_x = point[0], point[0]
-            min_y, max_y = point[1], point[1]
-
-    return (min_x, max_x), (min_y, max_y)
-
-def slice_json(json):
-    detections = detect_boundaries(json)
-    
-    min_x, max_x = detections[0][0], detections[0][1]
-    min_y, max_y = detections[1][0], detections[1][1]
-    
-    slice_amount = 3
-    
-    slice_width = (max_x - min_x) / slice_amount
-    slice_depth = max_y - min_y
-
-    boundary_1 = LineString([(min_x + slice_width, min_y), (min_x + slice_width, max_y)])
-    boundary_2 = LineString([(max_x - slice_width, min_y), (max_x - slice_width, max_y)])
-
-    boundaries = MultiLineString([boundary_1, boundary_2])
-
+def slice_json(json, slices = 3):
     fig = plt.figure()
     ax = fig.gca(projection="3d")
+    
+    min_x = min(line["points"][1][0] for line in json["lines"])
+    max_x = max(line["points"][1][0] for line in json["lines"])
+    
+    min_y = min(line["points"][1][1] for line in json["lines"])
+    max_y = max(line["points"][1][1] for line in json["lines"])
+    
+    slice_width = (max_x - min_x) / slices
+    boundaries = []
 
-    for boundary in list(boundaries.geoms):
-        boundary_coordinates = list(boundary.coords)
-        
-        bx1 = boundary_coordinates[0][0]
-        by1 = boundary_coordinates[0][1]
-        
-        bx2 = boundary_coordinates[1][0]
-        by2 = boundary_coordinates[1][1]
-        
+    for i in range(1, slices):
+        boundary = LineString([(min_x + i*slice_width, min_y), (min_x + i*slice_width, max_y)])
+        boundaries.append(boundary)
+
+        # To Visualize:
+        bound_coords = list(boundary.coords)
+        bx1, by1, bx2, by2 = bound_coords[0][0], bound_coords[0][1], bound_coords[1][0], bound_coords[1][1]
         ax.plot((bx1, bx2), (by1, by2), color="gray")
+
+    boundaries = MultiLineString(boundaries)
 
     for line in json["lines"]:
         lx1, ly1 = line["points"][0][0], line["points"][0][1]
         lx2, ly2 = line["points"][1][0], line["points"][1][1]
         
         line_str = LineString([(lx1,ly1), (lx2,ly2)])
-        result = split(line_str, boundaries)
+        sublines = split(line_str, boundaries)
 
-        for i, subline in enumerate(list(result.geoms)):
-            subline_coordinates = list(subline.coords)
+        for subline in list(sublines.geoms):
+            subline_coords = list(subline.coords)
             
-            sx1 = subline_coordinates[0][0]
-            sy1 = subline_coordinates[0][1]
+            sx1 = subline_coords[0][0]
+            sy1 = subline_coords[0][1]
             
-            sx2 = subline_coordinates[1][0]
-            sy2 = subline_coordinates[1][1]
+            sx2 = subline_coords[1][0]
+            sy2 = subline_coords[1][1] 
 
             if sx1 <= (min_x + slice_width) and sx2 <= (min_x + slice_width):
                 ax.plot(
@@ -89,7 +66,4 @@ def slice_json(json):
                     color = colors[line["type"]]
                 )   
 
-
-    fig.show()
-
-slice_json(parse(gcode_file))
+    return fig
--- code/test.py
+++ code/test.py
@@ -1,23 +1,11 @@
 from fab_visualizer import visualize
 from fab_estimator import estimate
+from fab_slicer import slice_json
 from fab_parser import parse
 
 samples_folder = "./../gcode_samples/"
 g_code = samples_folder + "1_peca.gcode"
 
 json = parse(g_code)
-plt = visualize(json)
-estimation = estimate(json)
-
-plt.show()
-print(f"- Estimated Print Time: {estimation}")
-
-'''
-parsing_runtime = parse(g_code, True)
-visualization_runtime = visualize(json, True)
-estimation_runtime = estimate(json, True)
-
-print(f"""- Parsing Runtime: {parsing_runtime}s
-- Estimation Runtime: {estimation_runtime}s
-- Visualization Runtime: {visualization_runtime}s""")
-'''
+fig_1 = slice_json(json)
+fig_1.show()
