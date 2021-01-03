e2c2b671ba0a4c2c5d489037f6deb77a9a24c56f
António <apgoncalves@outlook.pt>
2021-01-03 14:49:39 +0000

    Make Slicer return valid JSON

--- code/fab_slicer.py
+++ code/fab_slicer.py
@@ -1,26 +1,8 @@
 from shapely.geometry import LineString, MultiLineString
 from shapely.ops import split
-import matplotlib.pyplot as plt
-from fab_parser import parse
-
-# COLOR NAMES: https://matplotlib.org/3.1.0/gallery/color/named_colors.html
-colors = {
-    "SETUP": "orange",
-    "SUPPORT": "blue",
-    "TRAVEL": "black",
-    "SKIRT": "green",
-    "WALL-OUTER": "red",
-    "WALL-INNER": "cyan",
-    "SKIN": "magenta",
-    "FILL": "gold"
-}
-
-samples_folder = "./../gcode_samples/"
-gcode_file = samples_folder + "1_peca.gcode"
 
 def slice_json(json, slices = 3):
-    fig = plt.figure()
-    ax = fig.gca(projection="3d")
+    output = {}
     
     min_x = min(line["points"][1][0] for line in json["lines"])
     max_x = max(line["points"][1][0] for line in json["lines"])
@@ -35,35 +17,34 @@ def slice_json(json, slices = 3):
         boundary = LineString([(min_x + i*slice_width, min_y), (min_x + i*slice_width, max_y)])
         boundaries.append(boundary)
 
-        # To Visualize:
-        bound_coords = list(boundary.coords)
-        bx1, by1, bx2, by2 = bound_coords[0][0], bound_coords[0][1], bound_coords[1][0], bound_coords[1][1]
-        ax.plot((bx1, bx2), (by1, by2), color="gray")
-
     boundaries = MultiLineString(boundaries)
 
     for line in json["lines"]:
         lx1, ly1 = line["points"][0][0], line["points"][0][1]
         lx2, ly2 = line["points"][1][0], line["points"][1][1]
-        
+            
         line_str = LineString([(lx1,ly1), (lx2,ly2)])
         sublines = split(line_str, boundaries)
 
         for subline in list(sublines.geoms):
             subline_coords = list(subline.coords)
-            
+                
             sx1 = subline_coords[0][0]
             sy1 = subline_coords[0][1]
-            
+                
             sx2 = subline_coords[1][0]
-            sy2 = subline_coords[1][1] 
+            sy2 = subline_coords[1][1]
 
-            if sx1 <= (min_x + slice_width) and sx2 <= (min_x + slice_width):
-                ax.plot(
-                    [sx1, sx2],
-                    [sy1, sy2],
-                    [line["points"][0][2], line["points"][1][2]],
-                    color = colors[line["type"]]
-                )   
+            for n in range(0, slices):
+                output.setdefault(f"slice-{n+1}", {"input": f"Slice {n+1} of {json['input']}", "lines": []})
+                
+                left_bound = min_x + n*slice_width
+                right_bound = min_x + (n+1)*slice_width
 
-    return fig
+                if left_bound <= sx1 <= right_bound and left_bound <= sx2 <= right_bound:
+                    subline = {"points": [(sx1,sy1, line["points"][0][2]),
+                                          (sx2,sy2, line["points"][1][2])],
+                               "type": line["type"]}
+                    output[f"slice-{n+1}"]["lines"].append(subline)
+
+    return output
--- code/test.py
+++ code/test.py
@@ -2,10 +2,16 @@ from fab_visualizer import visualize
 from fab_estimator import estimate
 from fab_slicer import slice_json
 from fab_parser import parse
+import json
 
 samples_folder = "./../gcode_samples/"
-g_code = samples_folder + "1_peca.gcode"
+gcode_file = samples_folder + "1_peca.gcode"
 
-json = parse(g_code)
-fig_1 = slice_json(json)
-fig_1.show()
+json_data = parse(gcode_file)
+main_fig = visualize(json_data)
+main_fig.show()
+
+slices = slice_json(json_data)
+for key in slices:
+    temp_fig = visualize(slices[key])
+    temp_fig.show()
