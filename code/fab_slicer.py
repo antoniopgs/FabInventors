4783fee154c165dfa45a40dda210367af728639d
António <apgoncalves@outlook.pt>
2021-01-04 19:52:12 +0000

    Implement Grid Polygons in Slicer

--- code/fab_slicer.py
+++ code/fab_slicer.py
@@ -1,65 +1,63 @@
-from shapely.geometry import LineString, MultiLineString
-from shapely.ops import split
+from shapely.geometry import LineString, Polygon, MultiPolygon
 
-def set_boundaries(boundaries):
-    pass
-
-def slice_json(json, slices = 3):
+def slice_json(json_data, rows = 2, columns = 3):
     output = {}
-    
-    min_x = min(line["points"][1][0] for line in json["lines"])
-    max_x = max(line["points"][1][0] for line in json["lines"])
-    
-    min_y = min(line["points"][1][1] for line in json["lines"])
-    max_y = max(line["points"][1][1] for line in json["lines"])
-    
-    slice_width = (max_x - min_x) / slices
-    boundaries = []
 
-    for i in range(1, slices):
-        boundary_x = min_x + i*slice_width
-        boundary = LineString([(boundary_x, min_y), (boundary_x, max_y)])
-        boundaries.append(boundary)
+    min_x = min(line["points"][1][0] for line in json_data["lines"])
+    max_x = max(line["points"][1][0] for line in json_data["lines"])
+        
+    min_y = min(line["points"][1][1] for line in json_data["lines"])
+    max_y = max(line["points"][1][1] for line in json_data["lines"])
 
-    boundaries = MultiLineString(boundaries)
+    slice_width = (max_x - min_x) / columns
+    slice_depth = (max_y - min_y) / rows
 
-    for line in json["lines"]:
+    boundary_polygons = []
+
+    for r in range(0, rows):
+        for c in range(0, columns):
+            bottom_left = (min_x + c*slice_width, min_y + r*slice_depth)
+            bottom_right = (min_x + (c+1)*slice_width, min_y + r*slice_depth)
+            top_right = (min_x + (c+1)*slice_width, min_y + (r+1)*slice_depth)
+            top_left = (min_x + c*slice_width, min_y + (r+1)*slice_depth)
+            
+            temp_slice = Polygon([bottom_left, bottom_right, top_right, top_left])
+            boundary_polygons.append(temp_slice)
+
+    boundary_polygons = MultiPolygon(boundary_polygons)
+
+    for line in json_data["lines"]:
+
         lx1, ly1 = line["points"][0][0], line["points"][0][1]
         lx2, ly2 = line["points"][1][0], line["points"][1][1]
             
         line_str = LineString([(lx1,ly1), (lx2,ly2)])
-        
-        sublines = split(line_str, boundaries)
 
-        for subline in list(sublines.geoms):
+        for i, boundary_slice in enumerate(boundary_polygons):
+            output.setdefault(f"slice-{i+1}", {"input": f"Slice {i+1} of {json_data['input']}", "lines": []})
+            sub_line_str = line_str.intersection(boundary_slice)
             
-            subline_coords = list(subline.coords)
-                
-            sx1 = subline_coords[0][0]
-            sy1 = subline_coords[0][1]
-                
-            sx2 = subline_coords[1][0]
-            sy2 = subline_coords[1][1]
+            if not sub_line_str.is_empty:
+                sub_line_str_coords = list(sub_line_str.coords)
 
-            for n in range(0, slices):
-                output.setdefault(f"slice-{n+1}", {"input": f"Slice {n+1} of {json['input']}", "lines": []})
-                
-                left_bound = min_x + n*slice_width
-                right_bound = min_x + (n+1)*slice_width
+                sx1 = sub_line_str_coords[0][0]
+                sy1 = sub_line_str_coords[0][1]
+                    
+                sx2 = sub_line_str_coords[1][0]
+                sy2 = sub_line_str_coords[1][1]
 
                 if line_str.length == 0:
-                    subline_extrusion = line["extrusion"]
+                    sub_line_str_extrusion = line["extrusion"]
                 else:
-                    subline_extrusion = line["extrusion"] / (line_str.length / subline.length)
+                    sub_line_str_extrusion = line["extrusion"] / (line_str.length / sub_line_str.length)
 
-                if left_bound <= sx1 <= right_bound and left_bound <= sx2 <= right_bound:
-                    json_line = {"points": [(sx1,sy1, line["points"][0][2]),
-                                          (sx2,sy2, line["points"][1][2])],
-                                 "speed": line["speed"],
-                                 "extrusion": subline_extrusion,
-                                 "type": line["type"],
-                                 "mesh": line["mesh"]}
-                    
-                    output[f"slice-{n+1}"]["lines"].append(json_line)
+                json_line = {"points": [(sx1,sy1, line["points"][0][2]),
+                                        (sx2,sy2, line["points"][1][2])],
+                             "speed": line["speed"],
+                             "extrusion": sub_line_str_extrusion,
+                             "type": line["type"],
+                             "mesh": line["mesh"]}
+
+                output[f"slice-{i+1}"]["lines"].append(json_line)
 
     return output
--- code/test.py
+++ code/test.py
@@ -3,25 +3,19 @@ from fab_estimator import estimate
 from fab_gcodifier import gcodify
 from fab_slicer import slice_json
 from fab_parser import parse
-import json
 
 samples_folder = "./../gcode_samples/"
-gcode_file = samples_folder + "1_peca_suporte.gcode"
+gcode_file = samples_folder + "2_pecas.gcode"
 
 json_data = parse(gcode_file)
 
-main_fig = visualize(json_data)
-main_fig.show()
+slices = slice_json(json_data, rows=2, columns=3)
 
-#slices = slice_json(json_data, slices=3)
-
-'''
-chosen_slice = slices["slice-1"]
+chosen_slice = slices["slice-3"]
 gcodify(chosen_slice)
 
 slice_file = chosen_slice["input"]
 slice_data = parse(slice_file)
 
 slice_fig = visualize(slice_data)
-slice_fig.show() # Skirts are weird.
-'''
+slice_fig.show()
